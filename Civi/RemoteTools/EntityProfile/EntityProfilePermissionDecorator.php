da25d4a83782a4255418fa2017079e83ce08bc95
Dominic Tubach <tubach@systopia.de>
2023-05-12 12:28:28 +0200

    Add option suffixes as extra field

--- Civi/RemoteTools/EntityProfile/EntityProfilePermissionDecorator.php
+++ Civi/RemoteTools/EntityProfile/EntityProfilePermissionDecorator.php
@@ -19,167 +19,51 @@ declare(strict_types = 1);
 
 namespace Civi\RemoteTools\EntityProfile;
 
-use Civi\RemoteTools\Api4\Query\ConditionInterface;
 use CRM_Remotetools_ExtensionUtil as E;
 
 /**
  * Adds the fields CAN_delete and CAN_update.
  */
-final class EntityProfilePermissionDecorator implements RemoteEntityProfileInterface {
-
-  private RemoteEntityProfileInterface $profile;
-
-  public function __construct(RemoteEntityProfileInterface $profile) {
-    $this->profile = $profile;
-  }
-
-  public function getEntityName(): string {
-    return $this->profile->getEntityName();
-  }
-
-  public function getName(): string {
-    return $this->profile->getName();
-  }
-
-  public function getRemoteEntityName(): string {
-    return $this->profile->getRemoteEntityName();
-  }
+final class EntityProfilePermissionDecorator extends AbstractRemoteEntityProfileDecorator {
 
   /**
    * @inheritDoc
    */
   public function getRemoteFields(array $entityFields): array {
-    $remoteFields = $this->profile->getRemoteFields($entityFields);
+    $remoteFields = parent::getRemoteFields($entityFields);
     $remoteFields['CAN_delete'] = [
       'type' => 'Extra',
       'entity' => $this->profile->getRemoteEntityName(),
       'readonly' => TRUE,
       'name' => 'CAN_delete',
-      'title' => E::ts('Deletion Allowed'),
+      'title' => E::ts('Is Deletion Allowed'),
       'description' => E::ts('Is remote contact allowed to delete the record.'),
       'data_type' => 'Boolean',
-      'label' => E::ts('Deletion Allowed'),
+      'label' => E::ts('Is Deletion Allowed'),
     ];
     $remoteFields['CAN_update'] = [
       'type' => 'Extra',
       'entity' => $this->profile->getRemoteEntityName(),
       'readonly' => TRUE,
       'name' => 'CAN_update',
-      'title' => E::ts('Update Allowed'),
+      'title' => E::ts('Is Update Allowed'),
       'description' => E::ts('Is remote contact allowed to update the record.'),
       'data_type' => 'Boolean',
-      'label' => E::ts('Update Allowed'),
+      'label' => E::ts('Is Update Allowed'),
     ];
 
     return $remoteFields;
-  }
-
-  public function isImplicitJoinAllowed(string $fieldName, string $joinFieldName, ?int $contactId): bool {
-    return $this->profile->isImplicitJoinAllowed($fieldName, $joinFieldName, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function getSelectFieldNames(
-    array $select,
-    string $action,
-    array $remoteSelect,
-    ?int $contactId
-  ): array {
-    return $this->profile->getSelectFieldNames($select, $action, $remoteSelect, $contactId);
-  }
-
-  public function getFilter(?int $contactId): ?ConditionInterface {
-    return $this->profile->getFilter($contactId);
   }
 
   /**
    * @inheritDoc
    */
   public function convertToRemoteValues(array $entityValues, ?int $contactId): array {
-    $remoteValues = $this->profile->convertToRemoteValues($entityValues, $contactId);
+    $remoteValues = parent::convertToRemoteValues($entityValues, $contactId);
     $remoteValues['CAN_delete'] = $this->profile->isDeleteAllowed($entityValues, $contactId);
     $remoteValues['CAN_update'] = $this->profile->isUpdateAllowed($entityValues, $contactId);
 
     return $remoteValues;
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function getCreateFormSpec(array $arguments, array $entityFields, ?int $contactId): FormSpec {
-    return $this->profile->getCreateFormSpec($arguments, $entityFields, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function getUpdateFormSpec(array $entityValues, array $entityFields, ?int $contactId): FormSpec {
-    return $this->profile->getUpdateFormSpec($entityValues, $entityFields, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function isFormSpecNeedsFieldOptions(): bool {
-    return $this->profile->isFormSpecNeedsFieldOptions();
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function isCreateAllowed(array $arguments, ?int $contactId): bool {
-    return $this->profile->isCreateAllowed($arguments, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function isDeleteAllowed(array $entityValues, ?int $contactId): bool {
-    return $this->profile->isDeleteAllowed($entityValues, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function isUpdateAllowed(array $entityValues, ?int $contactId): bool {
-    return $this->profile->isUpdateAllowed($entityValues, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function validateCreateData(array $formData, ?int $contactId): ValidationResult {
-    return $this->profile->validateCreateData($formData, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function validateUpdateData(array $formData, array $currentEntityValues, ?int $contactId): ValidationResult {
-    return $this->profile->validateUpdateData($formData, $currentEntityValues, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function convertCreateDataToEntityValues(array $formData, ?int $contactId): array {
-    return $this->profile->convertCreateDataToEntityValues($formData, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function convertUpdateDataToEntityValues(array $formData, ?int $contactId): array {
-    return $this->profile->convertUpdateDataToEntityValues($formData, $contactId);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function convertToFormData(array $entityValues, ?int $contactId): array {
-    return $this->profile->convertToFormData($entityValues, $contactId);
   }
 
 }
