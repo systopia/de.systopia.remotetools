daff75e99ccc5850f99db064e3d2d3ca51c33b81
Dominic Tubach <tubach@systopia.de>
2023-06-13 11:56:13 +0200

    OptionFieldFactory: Use option labels

--- Civi/RemoteTools/Form/FormSpec/Field/AbstractOptionField.php
+++ Civi/RemoteTools/Form/FormSpec/Field/AbstractOptionField.php
@@ -6,7 +6,7 @@ namespace Civi\RemoteTools\Form\FormSpec\Field;
 use Civi\RemoteTools\Form\FormSpec\AbstractFormField;
 
 /**
- * @template T of scalar
+ * @template T of int|string
  *
  * @extends AbstractFormField<T>
  *
@@ -15,44 +15,40 @@ use Civi\RemoteTools\Form\FormSpec\AbstractFormField;
 abstract class AbstractOptionField extends AbstractFormField {
 
   /**
-   * @phpstan-var array<string, T>
-   *   Maps labels to values.
+   * @phpstan-var array<T, string>
+   *   Maps values to labels.
    */
   private array $options;
 
   /**
-   * @param array<string, T> $options
-   *   Maps labels to values.
+   * @param array<T, string> $options
+   *   Maps values to labels.
    */
   public function __construct(string $name, string $label, array $options) {
     parent::__construct($name, $label);
     $this->options = $options;
   }
 
-  public function isNullable(): bool {
-    return in_array(NULL, $this->getOptions(), TRUE) || parent::isNullable();
-  }
-
   /**
    * @param T $value
    */
-  public function addOption(string $label, $value): self {
-    $this->options[$label] = $value;
+  public function addOption($value, string $label): self {
+    $this->options[$value] = $label;
 
     return $this;
   }
 
   /**
-   * @phpstan-return array<string, T>
-   *   Maps labels to values.
+   * @phpstan-return array<T, string>
+   *   Maps values to labels.
    */
   public function getOptions(): array {
     return $this->options;
   }
 
   /**
-   * @param array<string, T> $options
-   *   Maps labels to values.
+   * @param array<T, string> $options
+   *   Maps values to labels.
    *
    * @return $this
    */
