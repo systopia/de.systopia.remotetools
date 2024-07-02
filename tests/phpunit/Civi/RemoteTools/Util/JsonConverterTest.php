9c2b8322c8af397a9b62ab9890a9506058cc26a4
Dominic Tubach <tubach@systopia.de>
2024-07-02 16:50:39 +0200

    JsonConverter: Do not convert floats with zero fraction to ints

--- tests/phpunit/Civi/RemoteTools/Util/JsonConverterTest.php
+++ tests/phpunit/Civi/RemoteTools/Util/JsonConverterTest.php
@@ -13,15 +13,17 @@ final class JsonConverterTest extends TestCase {
   public function testToArray(): void {
     $object = new \stdClass();
     $object->foo = 'bar';
+    $object->bar = 2.0;
 
-    static::assertSame(['foo' => 'bar'], JsonConverter::toArray($object));
+    static::assertSame(['foo' => 'bar', 'bar' => 2.0], JsonConverter::toArray($object));
   }
 
   public function testToStdClass(): void {
-    $array = ['foo' => 'bar'];
+    $array = ['foo' => 'bar', 'bar' => 2.0];
     $object = JsonConverter::toStdClass($array);
 
     static::assertSame('bar', $object->foo);
+    static::assertSame(2.0, $object->bar);
   }
 
   public function testToStdClassEmptyArray(): void {
