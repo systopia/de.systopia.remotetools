5602b13f02cbccae1e847928db7a53d4925c0209
Dominic Tubach <tubach@systopia.de>
2024-11-19 16:58:25 +0100

    Add support for vertical tabs in remote forms

---
+++ Civi/RemoteTools/JsonForms/FormSpec/Factory/Layout/CategoryFactory.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright (C) 2024 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\RemoteTools\JsonForms\FormSpec\Factory\Layout;
+
+use Civi\RemoteTools\Form\FormSpec\FormElementInterface;
+use Civi\RemoteTools\Form\FormSpec\FormTab;
+use Civi\RemoteTools\JsonForms\FormSpec\ElementUiSchemaFactoryInterface;
+use Civi\RemoteTools\JsonForms\FormSpec\Factory\AbstractConcreteElementUiSchemaFactory;
+use Civi\RemoteTools\JsonForms\JsonFormsElement;
+use Civi\RemoteTools\JsonForms\Layout\JsonFormsCategory;
+use Webmozart\Assert\Assert;
+
+final class CategoryFactory extends AbstractConcreteElementUiSchemaFactory {
+
+  public function createSchema(
+    FormElementInterface $element,
+    ElementUiSchemaFactoryInterface $factory
+  ): JsonFormsElement {
+    Assert::isInstanceOf($element, FormTab::class);
+    /** @var \Civi\RemoteTools\Form\FormSpec\FormTab $element */
+    $elements = array_map([$factory, 'createSchema'], $element->getElements());
+
+    return new JsonFormsCategory($element->getTitle(), $elements, $element->getDescription());
+  }
+
+  public function supportsElement(FormElementInterface $element): bool {
+    return $element instanceof FormTab;
+  }
+
+}
